48b29aa2aed240d01f5a6273f3d61e91cbe217af
Pranav Gaikwad <pgaikwad@redhat.com>
2019-08-19 14:56:44 -0400

    added delete environment optoin

--- demos/2019_RHTE/keynote/delete_rhte_env.sh
+++ demos/2019_RHTE/keynote/delete_rhte_env.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Deleting OCP3 environment
+OUR_DIR=`pwd`
 
+if [[ -z "${AGNOSTICD_HOME}" ]]; then
+    echo "Please ensure that 'AGNOSTICD_HOME' is set before running."
+    exit
+fi
 
-# Deleting OCP4 environment
+pushd .
+cd ${AGNOSTICD_HOME}
 
+echo "Deleting OCP3 environment..."
+ansible-playbook ${AGNOSTICD_HOME}/ansible/configs/ocp-workshop/destroy_env.yml -e @${OUR_DIR}/3.x/my_vars.yml -e @${OUR_DIR}/3.x/ocp3_vars.yml -e @${OUR_DIR}/secret.yml
+
+echo "Deleting OCP4 environment..."
+ansible-playbook ${AGNOSTICD_HOME}/ansible/configs/ocp4-workshop/destroy_env.yml -e @${OUR_DIR}/4.x/my_vars.yml -e @${OUR_DIR}/4.x/ocp3_vars.yml -e @${OUR_DIR}/secret.yml
+popd
