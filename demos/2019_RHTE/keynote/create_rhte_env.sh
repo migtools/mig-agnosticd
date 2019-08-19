70228dc1d69e20a825609e53c055f8a9e541fb4f
Pranav Gaikwad <pgaikwad@redhat.com>
2019-08-19 16:36:02 -0400

    making deployment and deletion scripts run in parallel

--- demos/2019_RHTE/keynote/create_rhte_env.sh
+++ demos/2019_RHTE/keynote/create_rhte_env.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+INFO='\033[0;32m'
+NC='\033[0m'
+
+info() {
+	echo -e "${INFO}${1}${NC}"
+}
+
 if [[ -z "${AGNOSTICD_HOME}" ]]; then
-    echo "Please ensure that 'AGNOSTICD_HOME' is set before running."
+    error "Please ensure that 'AGNOSTICD_HOME' is set before running."
     exit
 fi
 
@@ -10,8 +17,10 @@ pushd ${OUR_DIR}
 cd ${AGNOSTICD_HOME}
 
 echo "Creating OCP3 env..."
-ansible-playbook ${AGNOSTICD_HOME}/ansible/main.yml -e @${OUR_DIR}/3.x/my_vars.yml -e @${OUR_DIR}/3.x/ocp3_vars.yml -e @${OUR_DIR}/secret.yml
+ansible-playbook ${AGNOSTICD_HOME}/ansible/main.yml -e @${OUR_DIR}/3.x/my_vars.yml -e @${OUR_DIR}/3.x/ocp3_vars.yml -e @${OUR_DIR}/secret.yml &> ${OUR_DIR}/ocp3.log &
+info "Run 'tail -f ${OUR_DIR}/ocp3.log' for deployment logs"
 
 echo "Creating OCP4 env..."
-ansible-playbook ${AGNOSTICD_HOME}/ansible/main.yml -e @${OUR_DIR}/4.x/my_vars.yml -e @${OUR_DIR}/4.x/ocp4_vars.yml -e @${OUR_DIR}/secret.yml
+ansible-playbook ${AGNOSTICD_HOME}/ansible/main.yml -e @${OUR_DIR}/4.x/my_vars.yml -e @${OUR_DIR}/4.x/ocp4_vars.yml -e @${OUR_DIR}/secret.yml &> ${OUR_DIR}/ocp4.log &
+info "Run 'tail -f ${OUR_DIR}/ocp4.log' for deployment logs"
 popd
--- demos/2019_RHTE/keynote/delete_rhte_env.sh
+++ demos/2019_RHTE/keynote/delete_rhte_env.sh
@@ -2,6 +2,13 @@
 
 OUR_DIR=`pwd`
 
+INFO='\033[0;32m'
+NC='\033[0m'
+
+info() {
+        echo -e "${INFO}${1}${NC}"
+}
+
 if [[ -z "${AGNOSTICD_HOME}" ]]; then
     echo "Please ensure that 'AGNOSTICD_HOME' is set before running."
     exit
@@ -11,8 +18,10 @@ pushd ${OUR_DIR}
 cd ${AGNOSTICD_HOME}
 
 echo "Deleting OCP3 environment..."
-ansible-playbook ${AGNOSTICD_HOME}/ansible/configs/ocp-workshop/destroy_env.yml -e @${OUR_DIR}/3.x/my_vars.yml -e @${OUR_DIR}/3.x/ocp3_vars.yml -e @${OUR_DIR}/secret.yml
+ansible-playbook ${AGNOSTICD_HOME}/ansible/configs/ocp-workshop/destroy_env.yml -e @${OUR_DIR}/3.x/my_vars.yml -e @${OUR_DIR}/3.x/ocp3_vars.yml -e @${OUR_DIR}/secret.yml &> ${OUR_DIR}/ocp3.delete.log &
+info "Run 'tail -f ${OUR_DIR}/ocp3.delete.log' for deletion logs"
 
 echo "Deleting OCP4 environment..."
-ansible-playbook ${AGNOSTICD_HOME}/ansible/configs/ocp4-workshop/destroy_env.yml -e @${OUR_DIR}/4.x/my_vars.yml -e @${OUR_DIR}/4.x/ocp3_vars.yml -e @${OUR_DIR}/secret.yml
+ansible-playbook ${AGNOSTICD_HOME}/ansible/configs/ocp4-workshop/destroy_env.yml -e @${OUR_DIR}/4.x/my_vars.yml -e @${OUR_DIR}/4.x/ocp3_vars.yml -e @${OUR_DIR}/secret.yml &> ${OUR_DIR}/ocp4.delete.log &
+info "Run 'tail -f ${OUR_DIR}/ocp4.delete.log' for deletion logs"
 popd
