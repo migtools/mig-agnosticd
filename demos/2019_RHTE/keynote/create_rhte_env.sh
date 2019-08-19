4662586182620f266fb104a7288e4abd36d33de9
Pranav Gaikwad <pgaikwad@redhat.com>
2019-08-19 13:53:05 -0400

    updated variable files

--- demos/2019_RHTE/keynote/create_rhte_env.sh
+++ demos/2019_RHTE/keynote/create_rhte_env.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-echo "Creating OCP3 env..."
+if [[ -z "${AGNOSTICD_HOME}" ]]; then
+    echo "Please ensure that 'AGNOSTICD_HOME' is set before running."
+    exit
+fi
 
+OUR_DIR=`pwd`
+pushd .
+cd ${AGNOSTICD_HOME}
+
+echo "Creating OCP3 env..."
+ansible-playbook ${AGNOSTICD_HOME}/ansible/main.yml -e @${OUR_DIR}/3.x/my_vars.yml -e @${OUR_DIR}/3.x/ocp3_vars.yml -e @${OUR_DIR}/secret.yml
 
 echo "Creating OCP4 env..."
+ansible-playbook ${AGNOSTICD_HOME}/ansible/main.yml -e @${OUR_DIR}/4.x/my_vars.yml -e @${OUR_DIR}/4.x/ocp4_vars.yml -e @${OUR_DIR}/secret.yml
+popd
