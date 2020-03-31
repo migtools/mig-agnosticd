4eb3247a64caa91f9fcb54a25a418899254caddd
Pranav Gaikwad <pgaikwad@redhat.com>
2020-03-31 10:03:01 -0400

    changed back to summit repo

--- demos/2020_Summit/tests/4.x/create_ocp4_workshop.sh
+++ demos/2020_Summit/tests/4.x/create_ocp4_workshop.sh
@@ -7,5 +7,5 @@ fi
 
 pushd .
 cd ${AGNOSTICD_HOME} 
-ansible-playbook ${AGNOSTICD_HOME}/ansible/main.yml -e @${OUR_DIR}/my_vars.yml -e @${OUR_DIR}/ocp4_vars.yml -e @${OUR_DIR}/../../../../secret.yml   
+ansible-playbook ${AGNOSTICD_HOME}/ansible/main.yml -e @${OUR_DIR}/my_vars.yml -e @${OUR_DIR}/ocp4_vars.yml -e @${OUR_DIR}/../../../../secret.yml --skip-tags step003,step004,infra_workloads,student_workloads,post_flight_check
 popd 
