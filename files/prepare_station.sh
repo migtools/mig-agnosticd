58a524dc0970d9a453aca1dc771558b6bf1beb7e
Nenad Peric <nenad@redhat.com>
2021-04-05 13:17:30 +0200

    Fix prepare guid (#153)
    * Reducing some checks, adding GUID to block
    
    * Add GUID to Ansible part, minor changes to entry

--- files/prepare_station.sh
+++ files/prepare_station.sh
@@ -12,12 +12,14 @@ PASSWORD=PASSWORD
 API_LOGIN=API_LOGIN
 API_PASS=API_PASS
 API_ADDRESS=API_ADDRESS
+LOCAL_GUID=GUID
 # END ANSIBLE MANAGED BLOCK
 
 main(){
     
 # Print a welcome message and ask user for input
-welcome_message_guid
+welcome_message
+check_guid
 # Try to reach the OCP 3 cluster, and copy the cluster.info over
 get_cluster_info
 
@@ -34,9 +36,9 @@ deploy_bookbag
 # Functions go here
 # Function which welcomes the user and asks for OCP3 hostname
 
-welcome_message_guid() {
-clear
-cat << EOF
+welcome_message() {
+    clear
+    cat << EOF
     ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
     ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
     ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
@@ -60,35 +62,30 @@ cat << EOF
 
    =================================================================                                                              
 EOF
-printf "\nPlease enter your OCP3 bastion hostname. \nThat is the one you received for SSH into OCP3 environment: "
-check_hostname
-
-# Check if the GUID is from the LOCAL system
-while [ $GUID = $LOCAL_GUID ]
-do
-    printf "Please enter the OCP3 hostname, not the OCP4 (local) system hostname: "
+    printf "\nPlease enter your OCP3 bastion hostname. \nThat is the one you received FOR YOUR OCP3 environment: "
     check_hostname
-done
-printf "Your OCP3 GUID is $GUID. Working...\n"
 }
 
-
+check_guid(){
+    # Check if the GUID is from the LOCAL system
+    while [ $GUID = $LOCAL_GUID ]
+    do
+        printf "\nPlease enter the OCP3 hostname, NOT the OCP4 (local) system hostname: "
+        check_hostname
+    done
+    printf "Your OCP3 GUID is $GUID. \nWorking...\n"
+}
 
 check_hostname(){
-    read SSH_HOSTNAME
-    if [[ HOSTNAME == "@" ]]
+    read HOSTNAME
+    if [[ "$HOSTNAME" =~ "@" ]]
         then
             # Someone pasted the whole thing, with the username, strip it
-            HOSTNAME=$(echo $SSH_HOSTNAME|cut -d @ -f 2)
-        else
-            HOSTNAME=$SSH_HOSTNAME
+            HOSTNAME=$(echo $HOSTNAME|cut -d @ -f 2)
     fi 
     GUID=$(echo $HOSTNAME|cut -d . -f 2)
-    printf "GUID: $GUID\n"
-
+    # printf "GUID: $GUID\n"
 }
-
-# Getting and merging the cluster.info files. 
 
 get_cluster_info(){
     printf "Checking cluster connectivity\n"
@@ -98,18 +95,15 @@ get_cluster_info(){
     printf "Host still not reachable. Waiting 15s and trying again\n"
     sleep 15
     done
-    
+    # Getting and merging the cluster.info files. 
     printf "Grabbing cluster info from OCP3 cluster\n"
     sshpass -p "$PASSWORD" scp $STUDENT@$SSH_HOSTNAME:./cluster.info cluster.ocp3
     cat cluster.ocp3 >> cluster.info
-
 }
 
 deploy_bookbag(){
     # We have to oc login to be able to make changes to the cluster
     oc login -u $API_LOGIN -p $API_PASS --insecure-skip-tls-verify=true $API_ADDRESS
-
-
 }
 
 main "$@"
