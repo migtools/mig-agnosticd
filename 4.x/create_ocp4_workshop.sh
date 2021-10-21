8c5c0a44a351ba1c9a451d7fca099114b6d5d7f4
John Matthews <jwmatthews@gmail.com>
2021-10-21 07:26:15 -0400

    Updates from virtualenv test with MacOS and ocp4-cluster (#185)
    * Updates from virtualenv test with MacOS and ocp4-cluster
    
    * Added check for Darwin

--- 4.x/create_ocp4_workshop.sh
+++ 4.x/create_ocp4_workshop.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
 OUR_DIR=`pwd`
+
+OS="$(uname)"
+if [ "$OS" = "Darwin" ]; then
+  # Required for MacOS with virtualenv
+  # as per https://github.com/konveyor/mig-agnosticd/issues/182
+  export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+fi
 
 if [[ -z "${AGNOSTICD_HOME}" ]]; then
     echo "Please ensure that 'AGNOSTICD_HOME' is set before running."
--- 4.x/delete_ocp4_workshop.sh
+++ 4.x/delete_ocp4_workshop.sh
@@ -1,5 +1,12 @@
 OUR_DIR=`pwd`
 
+OS="$(uname)"
+if [ "$OS" = "Darwin" ]; then
+  # Required for MacOS with virtualenv
+  # as per https://github.com/konveyor/mig-agnosticd/issues/182
+  export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+fi
+
 if [[ -z "${AGNOSTICD_HOME}" ]]; then
     echo "Please ensure that 'AGNOSTICD_HOME' is set before running."
     exit
